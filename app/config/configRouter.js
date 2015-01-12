91c943a7879c19858013375b5dd7dda85a00f373
Tong Xiang <tong.xiang2@gmail.com>
2015-01-12 16:48:10 -0500

    intermediate commit for cms backend routing stuff

--- app/config/configRouter.js
+++ app/config/configRouter.js
@@ -16,16 +16,47 @@ var express = require('express')
     ]
   ;
 
-router.post('/:configCollectionName', function(req, res) {
-  console.log('Config collection post request made for collection: ' + req.body['__comments']);
-  for (var i = 0; i < configModelArray.length; i++) {
-    if (configModelArray[i].modelName == req.params.configCollectionName) {
-      configModelArray[i].findOneAndUpdate({'_id': req.body['_id']}, req.body, { upsert: true }, function(err, doc) {
-        res.send(doc);
-        console.log('new doc: ', doc)
+router.route('/')
+      .get(function(req, res) {
+        res.send('Eventually, maybe this will return all the config documents in a way that\'s useful for our CMS.')
       })
+
+router.route('/:configCollectionName')
+      . 
+
+router.use(function(req, res, next) {
+        logger.info('Request made for configRouter with contents: ' + req.body)
+      })
+
+      .post('/:configCollectionName', function(req, res) {
+        console.log('Config collection post request made for collection: ' + req.body.__comments);
+        findConfigCollection(req.params.configCollectionName).findOneAndUpdate({'_id': req.body._id}, req.body, { upsert: true }, function(err, doc) {
+          res.send(doc);
+          console.log('new doc: ', doc);
+        })
+      })
+
+      .delete('/:configCollectionName', function(req, res) {
+        findConfigCollection(req.params.configCollectionName).remove({
+          _id: req.body._id
+        }, function(err) {
+          if (err) {
+            logger.error('Unable to delete doc with _id ' + req.body._id + ' for configCollection: ' + req.params.configCollectionName)
+          }
+        })
+      })
+
+
+
+function findConfigCollection(configCollectionName) {
+  for (var i = 0; i < configModelArray.length; i++) {
+    if (configModelArray[i].modelName == configCollectionName) {
+      return configModelArray[i];
     }
   }
-})
+  logger.error('Unable to find requested config collection model for ' + configCollectionName);
+  return false;
+}
+
 
 module.exports = router;
